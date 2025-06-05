90bd33a2327b0e139c6d2cc751616eb80a5ec30b
joao-andre2003 <76053777+joao-andre2003@users.noreply.github.com>
2025-06-05 20:44:52 -0300

    Add files via upload

---
+++ Tools/AddNewMedidor.py
@@ -0,0 +1,121 @@
+# Projeto EEMEPP, UFPR - Autor: João André Agustinho da Silva 
+
+# Descrição: 
+
+import json
+from pyModbusTCP.client import ModbusClient
+import mysql.connector
+from mysql.connector import Error
+from getpass import getpass
+
+MagicaGrandePATH = "../MagicaGrande.json";
+
+PORT = 502;
+FEED_SIZES = [57, 297]; #Tamanhos fixos de feeds
+FEED_MODEL = {};
+FEED_MODEL_ENGINE = "11 " * max(FEED_SIZES);
+FEED_MODEL_INTERVAL = "60 " * max(FEED_SIZES);
+FEED_MODEL_OP_TYPE = "0 " * max(FEED_SIZES);
+FEED_MODEL_PUBLIC = "0 " * max(FEED_SIZES);
+FEED_MODEL_ENABLED = "1 " * max(FEED_SIZES);
+
+def GetFeedModel(filePath):
+    _FEED_MODEL = {}
+    with open(filePath, 'r+', encoding='utf-8') as file:
+        _content = file.read().replace(", \n", ', ""\n').split("\n");
+        for i, element in enumerate(_content):
+            print(element);
+        _content2 = (print(n) for i,n in enumerate(_content))
+        print(_content2)
+    return {}
+
+def main():
+    FEED_MODEL = GetFeedModel("./Feed_Model.txt");
+    return
+    """
+    print("< - - Inserir Novo Medidor ao UniversityCollider - - >")
+    name = input("Nome do Medidor: ");
+    
+    ipAdress = "";
+    while(True):
+        ipAdress = input("Endereço IP do Medidor: ");
+        _modbus = ModbusClient(host=ipAdress, port=PORT, unit_id=255)
+        if not _modbus.open():
+            print("ERRO: Conexão ModBus Recusada, IP pode estar errado.");
+            continue;
+        print("-> Conexão ModBus Aprovada. Medidor conectado.");
+        _modbus.close();
+        break;
+    
+    feed_list = [];
+    isHarmonico = False;
+    while(True):
+        _feed_list_str = input("Feeds desse Medidor (Números separados por espaço ou vírgula e espaço. Use ... para completar sequencia de números. Ex: 1 ... 10 para valores entre 1 e 10):\n").split();
+        for i in range(len(_feed_list_str)):
+            _feed_list_str[i] = _feed_list_str[i].replace(",", "");
+            if _feed_list_str[i] == "...":
+                feed_list += (n for n in list(range(int(_feed_list_str[i-1])+1, int(_feed_list_str[i+1]))));
+            elif _feed_list_str[i].isnumeric():
+                feed_list.append(int(_feed_list_str[i]));
+
+        if len(feed_list) not in FEED_SIZES:
+            print("ERRO: Tamanho de Feed incorreto.");
+            feed_list = [];
+            continue;
+        if len(feed_list) == max(FEED_SIZES):
+            print("AVISO: O Medidor foi configurado como um harmônico.");
+            isHarmonico = True;
+        break;
+    
+    new_medidor = {name: [ipAdress, feed_list, isHarmonico]} if isHarmonico else {name: [ipAdress, feed_list]};
+
+    updated_file = "";
+    with open(MagicaGrandePATH,'r+', encoding='utf-8') as file:
+        json.load(file)
+        json.dump(new_medidor, file, indent=None);
+    with open(MagicaGrandePATH, 'r', encoding='utf-8') as file:
+        updated_file = file.read().replace('\n}{', ',\n').replace('}{', ',\n');
+    with open(MagicaGrandePATH, 'w', encoding='utf-8') as file:
+        file.write(updated_file);
+
+    print("-> Arquivo "+MagicaGrandePATH+" Atualizado com Sucesso.");
+    """
+    with open("./Feed_Model.txt", 'w', encoding='utf-8') as f:
+        f.write("name, unit");
+        _aux = (" " * (FEED_SIZES[1] - FEED_SIZES[0]))
+        fEED_MODEL_UNIT = "V V V V V V V A A A A A Hz Hz Hz Hz W W W W VAr VAr VAr VAr VA VA VA VA          KWh KVArh KWh KWh KW KW KVA KVA            " + _aux;
+        fEED_MODEL_UNIT = fEED_MODEL_UNIT.split(" ");
+        fEED_MODEL_NAME = "U0 U12 U23 U31 U1 U2 U3 I0 IN I1 I2 I3 F1 F2 F3 F1IEC P0 P1 P2 P3 Q0 Q1 Q2 Q3 S0 S1 S2 S3 FP0 FP1 FP2 FP3 FP0D FP1D FP2D FP3D FATORK EApositivo ERpositivo EAnegativo ERnegativo MDA DA MDS DS UANTHD UBNTHD UCNTHD IATHD IBTHD ICTHD U1THDagrup U2THDagrup U3THDagrup I1THDagrup I2THDagrup I3THDagrup U1AgrupH1 U1AgrupH2 U1AgrupH3 U1AgrupH4 U1AgrupH5 U1AgrupH6 U1AgrupH7 U1AgrupH8 U1AgrupH9 U1AgrupH10 U1AgrupH11 U1AgrupH12 U1AgrupH13 U1AgrupH14 U1AgrupH15 U1AgrupH16 U1AgrupH17 U1AgrupH18 U1AgrupH19 U1AgrupH20 U1AgrupH21 U1AgrupH22 U1AgrupH23 U1AgrupH24 U1AgrupH25 U1AgrupH26 U1AgrupH27 U1AgrupH28 U1AgrupH29 U1AgrupH30 U1AgrupH31 U1AgrupH32 U1AgrupH33 U1AgrupH34 U1AgrupH35 U1AgrupH36 U1AgrupH37 U1AgrupH38 U1AgrupH39 U1AgrupH40 U2AgrupH1 U2AgrupH2 U2AgrupH3 U2AgrupH4 U2AgrupH5 U2AgrupH6 U2AgrupH7 U2AgrupH8 U2AgrupH9 U2AgrupH10 U2AgrupH11 U2AgrupH12 U2AgrupH13 U2AgrupH14 U2AgrupH15 U2AgrupH16 U2AgrupH17 U2AgrupH18 U2AgrupH19 U2AgrupH20 U2AgrupH21 U2AgrupH22 U2AgrupH23 U2AgrupH24 U2AgrupH25 U2AgrupH26 U2AgrupH27 U2AgrupH28 U2AgrupH29 U2AgrupH30 U2AgrupH31 U2AgrupH32 U2AgrupH33 U2AgrupH34 U2AgrupH35 U2AgrupH36 U2AgrupH37 U2AgrupH38 U2AgrupH39 U2AgrupH40 U3AgrupH1 U3AgrupH2 U3AgrupH3 U3AgrupH4 U3AgrupH5 U3AgrupH6 U3AgrupH7 U3AgrupH8 U3AgrupH9 U3AgrupH10 U3AgrupH11 U3AgrupH12 U3AgrupH13 U3AgrupH14 U3AgrupH15 U3AgrupH16 U3AgrupH17 U3AgrupH18 U3AgrupH19 U3AgrupH20 U3AgrupH21 U3AgrupH22 U3AgrupH23 U3AgrupH24 U3AgrupH25 U3AgrupH26 U3AgrupH27 U3AgrupH28 U3AgrupH29 U3AgrupH30 U3AgrupH31 U3AgrupH32 U3AgrupH33 U3AgrupH34 U3AgrupH35 U3AgrupH36 U3AgrupH37 U3AgrupH38 U3AgrupH39 U3AgrupH40 I1AgrupH1 I1AgrupH2 I1AgrupH3 I1AgrupH4 I1AgrupH5 I1AgrupH6 I1AgrupH7 I1AgrupH8 I1AgrupH9 I1AgrupH10 I1AgrupH11 I1AgrupH12 I1AgrupH13 I1AgrupH14 I1AgrupH15 I1AgrupH16 I1AgrupH17 I1AgrupH18 I1AgrupH19 I1AgrupH20 I1AgrupH21 I1AgrupH22 I1AgrupH23 I1AgrupH24 I1AgrupH25 I1AgrupH26 I1AgrupH27 I1AgrupH28 I1AgrupH29 I1AgrupH30 I1AgrupH31 I1AgrupH32 I1AgrupH33 I1AgrupH34 I1AgrupH35 I1AgrupH36 I1AgrupH37 I1AgrupH38 I1AgrupH39 I1AgrupH40 I2AgrupH1 I2AgrupH2 I2AgrupH3 I2AgrupH4 I2AgrupH5 I2AgrupH6 I2AgrupH7 I2AgrupH8 I2AgrupH9 I2AgrupH10 I2AgrupH11 I2AgrupH12 I2AgrupH13 I2AgrupH14 I2AgrupH15 I2AgrupH16 I2AgrupH17 I2AgrupH18 I2AgrupH19 I2AgrupH20 I2AgrupH21 I2AgrupH22 I2AgrupH23 I2AgrupH24 I2AgrupH25 I2AgrupH26 I2AgrupH27 I2AgrupH28 I2AgrupH29 I2AgrupH30 I2AgrupH31 I2AgrupH32 I2AgrupH33 I2AgrupH34 I2AgrupH35 I2AgrupH36 I2AgrupH37 I2AgrupH38 I2AgrupH39 I2AgrupH40 I3AgrupH1 I3AgrupH2 I3AgrupH3 I3AgrupH4 I3AgrupH5 I3AgrupH6 I3AgrupH7 I3AgrupH8 I3AgrupH9 I3AgrupH10 I3AgrupH11 I3AgrupH12 I3AgrupH13 I3AgrupH14 I3AgrupH15 I3AgrupH16 I3AgrupH17 I3AgrupH18 I3AgrupH19 I3AgrupH20 I3AgrupH21 I3AgrupH22 I3AgrupH23 I3AgrupH24 I3AgrupH25 I3AgrupH26 I3AgrupH27 I3AgrupH28 I3AgrupH29 I3AgrupH30 I3AgrupH31 I3AgrupH32 I3AgrupH33 I3AgrupH34 I3AgrupH35 I3AgrupH36 I3AgrupH37 I3AgrupH38 I3AgrupH39 I3AgrupH40".split(" ");
+        for n in range(297):
+            f.write("\n"+fEED_MODEL_NAME[n]+", "+fEED_MODEL_UNIT[n])
+
+
+    return;
+    print("\n< - - Iniciando Atualização do SQL Emoncms - - >");
+
+    db = None;
+    while(True):
+        _host = input("Host SQL (=localhost se deixado vazio): ");
+        _host = "localhost" if _host=="" else _host;
+        _database = input("Database SQL (=emoncms se deixado vazio): ");
+        _database = "localhost" if _database=="" else _database;
+        _user = input("Usuário SQL: ");
+        _password = getpass("Senha SQL: ");
+
+        try:
+            db = mysql.connector.connect(host=_host, user=_user, password=_password, database=_database);
+            if not db.is_connected():
+                print("ERRO: Conexão SQL não estabelecida. Cheque os dados novamente.");
+                continue;
+        except Error as e:
+            print(f"ERRO: {e}")
+            continue;
+        break;
+    print("-> Conexão SQL Estabelecida.")
+    cursor = db.cursor()
+
+
+
+    db.commit();
+    db.close();
+main();
---
+++ Tools/CheckMissingData.py
@@ -0,0 +1,203 @@
+# Projeto EEMEPP, UFPR - Autor: João André Agustinho da Silva 
+
+# Descrição: Código para ler os arquivos do diretório do SharePoint onde está todos os arquivos de dados de P0 e EApositivo extraídos da Canopus, verificar a data que está nos
+# nomes dos arquivos e identificar quais datas estão faltando para completar os dados até o dia de hoje. Logo após a identificação, usa uma versão modificada do código do 
+# discente Vitor Samuel Ruppel Ferreira, "data_extractor.ipynb", onde a extração de dados via API da Canopus é realizada e armazenada num arquivo CSV local, o qual é exportado 
+# para o Teams em seguida.
+
+import datetime
+import numpy as np
+import pandas as pd
+import requests
+from tqdm import tqdm
+import json
+import os
+from office365.runtime.auth.user_credential import UserCredential
+from office365.sharepoint.client_context import ClientContext
+from getpass import getpass
+
+SERVER = ["https://canopus.eletrica.ufpr.br", "dcb98cfc2d842511f0b26d49ce1dd32f"];
+
+def main():
+    #region - - - - - - - - - Login Sharepoint e Configuração - - - - - - - - - 
+
+    SP_username = "joao.andre@ufpr.br"; # Usuário do Sharepoint/Teams
+    SP_password = ""; # Senha do Sharepoint/Teams - DEIXE VAZIO SE QUISER FAZER LOGIN MANUAL NA EXECUÇÃO DO CÓDIGO
+
+    Export_file = True; # True se deseja a exportação do arquivo ao Sharepoint
+    Delete_local_file = True; # True se quer deletar o CSV local após a exportação
+
+    Sharepoint_url = "https://ufprbr0.sharepoint.com/sites/Projeto_CANOPUS2"; # URL com Sites incluso
+    Export_path = f'Documentos%20Compartilhados/General/PowerBI/CanopusDB_EAP_P0%20-%20Power%20BI/'; # Caminho onde vai ser enviado no SP
+
+    ctx = LoginSharepoint(Sharepoint_url, SP_username, SP_password);
+
+    #region - - - - - - - - - Importar e organizar formato de data - - - - - - - - - 
+
+    all_dates = [];
+    all_dates = GetDates(ctx, Export_path);
+
+    #region - - - - - - - - - Verificar se há datas de dados faltando e preencher - - - - - - - - - 
+
+    i = -1;
+    while i < len(all_dates) - 2:
+        i += 1;
+        _start_date = DatesFromFormat(all_dates[i])[1];
+        _final_date = DatesFromFormat(all_dates[i+1])[0];
+
+        _start_date_DT = datetime.datetime.strptime(_start_date, '%Y-%m-%d') + datetime.timedelta(1);
+        _final_date_DT = datetime.datetime.strptime(_final_date, '%Y-%m-%d') - datetime.timedelta(1);
+
+        if (_final_date_DT - _start_date_DT).days < 0:
+            continue;
+        
+        _start_date = datetime.datetime.strftime(_start_date_DT, '%d-%m-%Y');
+        _final_date = datetime.datetime.strftime(_final_date_DT, '%d-%m-%Y');
+        print("inicio: "+_start_date+" final: "+_final_date);
+
+        if _start_date_DT.year != _final_date_DT.year:
+            _final_date_DT = datetime.datetime(_start_date_DT.year, 12, 31);
+            all_dates = GetDates(ctx, Export_path);
+            i -= 1;
+
+        _file_name = f'CanopusDB_{_start_date if _start_date==_final_date else _start_date+"_"+_final_date}.csv'; # Nome do arquivo pra arquivo diários e em arquivos de datas maiores - Nome padronizado para o funcionamento desse código e do Power BI
+        _local_path = f'./{_file_name}'; # Onde o arquivo vai ser salvo na máquina local + nome
+        Export_path += str(_start_date_DT.year);
+        ImportDataFromDate(_start_date, _final_date, _local_path);   
+
+        if Export_file:
+            ExportData(_local_path, Export_path, _file_name, ctx, Delete_local_file);
+    
+        all_dates.sort();
+    return None;
+
+def GetDates(ctx, Export_path):
+    all_dates = [];
+    for folder in ctx.web.get_folder_by_server_relative_url(Export_path).folders.get_all().execute_query():
+        for file in ctx.web.get_folder_by_server_relative_url(Export_path + str(folder)).files.get_all().execute_query():
+            _date = str(file);
+            _date = _date[_date.find("_")+1 : _date.rfind(".")];
+
+            _date_split = _date.split(sep="-");
+            if len(_date_split) <= 3: # Reorganizar formato de data para YYYY-MM-DD
+                _date = _date_split[2]+"-"+_date_split[1]+"-"+_date_split[0]; # Se for uma faixa diária
+            else:
+                _date = _date_split[2][:4]+"-"+_date_split[1]+"-"+_date_split[0]+"_"+_date_split[4]+"-"+_date_split[3]+"-"+_date_split[2][5:]; # Se for uma faixa de tempo maior YYYY-MM-DD_YYYY-MM-DD
+            all_dates.append(_date);
+
+    all_dates.sort() # O formato YYYY-MM-DD ajuda a organizar de maneira correta
+    all_dates.append(datetime.datetime.today().strftime("%Y-%m-%d")); # Incluir o dia anterior (msm se o dia anterior já estiver incluso essa linha não resulta em erro)
+    return all_dates;
+
+def DatesFromFormat(date): # Verifica o formato do arquivo (data diária ou de período) e retorna o início e o fim da data
+    date = str(date);
+    start_final_date = [];
+    if len(date) <= 10:
+        start_final_date.append(date);
+        start_final_date.append(date);
+    else:
+        start_final_date = date.split(sep="_");
+    return start_final_date;
+
+def Get_Feeds_IDs_Tag_Name(): # Extrai ids, medidores e grandeza da Canopus para consulta
+    serverpath, apikey = SERVER;
+    urlFeeds = serverpath + "/feed/list.json&userid=1&apikey=" + apikey;
+
+    feeds = requests.get(urlFeeds);
+    lista_feeds = np.array(feeds.json());
+
+    lista_imt = [];
+    lista_params = ["EApositivo","P0"]; # Grandezas que vão ser extraídas
+    for item in lista_feeds:
+        if item["name"] in lista_params and item["tag"] not in lista_imt:
+            lista_imt.append((item["id"], item["name"], item["tag"]));
+    return lista_imt;
+
+def Get_Feed_Data_From_Server(feed_id, startUNIX, endUNIX, interval): # Extrai dados da Canopus
+    serverUrl, apikeyRead = SERVER;
+    url = serverUrl + "/feed/data.json?id=" + str(feed_id) + "&start=" + str(startUNIX) + "000&end=" + str(endUNIX) + "000&interval=" + str(interval) + "&apikey=" + apikeyRead;
+    dataRecuperada = requests.get(url);
+    json_data = None;
+
+    try:
+        json_data = dataRecuperada.json();
+    except:
+        print("ERRO PHP: "+str(dataRecuperada.text[: dataRecuperada.text.find("[")]));
+        if json_data != None:
+            json_data = json.loads(dataRecuperada.text[dataRecuperada.text.find("[") :]);
+        else:
+            return None;
+    try:
+        while json_data != [] and json_data[-1][0] > (endUNIX * 1000): # A importação da Canopus costuma vir com um dado após o tempo solicitado
+            json_data.pop(); 
+    except:
+        print("ERRO META: "+str(dataRecuperada.text));
+        return None;
+        
+    return json_data;
+
+def ImportDataFromDate(Start_date, Final_date, Local_path):
+    #region - - - - - - - - - Importar e criar arquivo CSV - - - - - - - - - 
+
+    Intervalo = 600; # Intervalo entre dados em segundos
+    IntervaloStep = Intervalo * 8900; # O valor limite de extração da API é 8928 pontos
+
+    UnixTime_inicial = datetime.datetime.strptime(Start_date, '%d-%m-%Y').timestamp();
+    UnixTime_final = (datetime.datetime.strptime(Final_date, '%d-%m-%Y') + datetime.timedelta(0, 59, 0, 0, 59, 23)).timestamp();
+
+    gmt = (int)(datetime.datetime.now().hour - datetime.datetime.now(datetime.timezone.utc).hour); # Fuso horário
+    UnixTime_inicial += gmt*3600;
+    UnixTime_final += gmt*3600;
+
+    df = pd.DataFrame(); # O arquivo que irá guardar os dados
+    Lista_feed = Get_Feeds_IDs_Tag_Name();
+    # FIM - Declarações
+
+    print("Iniciando importação da Canopus . . .");
+    for id, grandeza, medidor in tqdm(Lista_feed):
+        _unixTime_inicial = UnixTime_inicial;
+
+        while(_unixTime_inicial <= UnixTime_final): # Verifica o tamanho da faixa, se for muito grande extrai os dados por partes do tamanho de IntervaloStep
+            _unixTime_final = _unixTime_inicial + IntervaloStep if _unixTime_inicial + IntervaloStep < UnixTime_final else UnixTime_final;
+
+            dados_parcial = Get_Feed_Data_From_Server(id, int(_unixTime_inicial), int(_unixTime_final), Intervalo);
+            try:
+                df_parc  = pd.DataFrame(np.array(dados_parcial), columns = ['tempo','valor']);
+                df_parc['grandeza'] = len(list(df_parc['tempo']))*[grandeza];
+                df_parc['predio'] = len(list(df_parc['tempo']))*[medidor];
+                df_parc['data_Brasilia'] = pd.to_datetime(df_parc['tempo'], unit='ms');
+                df = pd.concat([df_parc, df]);
+            except Exception as e:
+                print(f'ERRO {e} ocorreu em {medidor}' if dados_parcial != [] else f'ERRO medidor {medidor} sem dados nessa data');
+
+            _unixTime_inicial += IntervaloStep + Intervalo;
+
+    df.to_csv(Local_path, index=False);
+    return None;
+
+def ExportData(Local_path, Export_path, File_name, client_ctx, Delete_local_file):
+    #region - - - - - - - - - Exportar para o Sharepoint - - - - - - - - - 
+    print("Iniciando exportação ao Sharepoint . . .");
+    with open(Local_path, 'rb') as _content_file:
+        _file_content = _content_file.read();
+
+    client_ctx.web.get_folder_by_server_relative_url(Export_path).upload_file(File_name, _file_content).execute_query();
+    print("Exportação Concluída.");   
+
+    if Delete_local_file:
+        os.remove(Local_path);
+        print("Arquivo CSV local excluído.");
+    return None;
+
+def LoginSharepoint(Sharepoint_url, SP_username, SP_password):
+    while True:
+        try:
+            _userCredentials = UserCredential(SP_username, SP_password);
+            _ctx = ClientContext(Sharepoint_url).with_credentials(_userCredentials);
+            _ctx.web.current_user.get().execute_query();
+            return _ctx;
+        except:
+            SP_username = input("Email usuário Teams: ");
+            SP_password = getpass("Senha Teams: ");
+
+main();
